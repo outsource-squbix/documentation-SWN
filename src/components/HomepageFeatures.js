72ad6a89a4f57fe3ea281c02584e2a380d433812
biswajit <biswajit.mohanty@squbix.com>
2021-12-17 15:52:40 +0530

    new commit

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,34 +4,20 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   { 
-    title: 'Powered by Markdown',
+    title: 'MetaMUI',
     Svg: require('../../static/img/11.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    
   },
   {
-    title: 'Built Using React',
+    title: 'SSID',
     Svg: require('../../static/img/22.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    
+    
   },
   {
-    title: 'Ready for Translations',
+    title: 'Pairwise',
     Svg: require('../../static/img/33.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    
   },
 ];
 
